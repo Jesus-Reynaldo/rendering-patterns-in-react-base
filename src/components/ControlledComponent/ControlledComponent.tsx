8f097d8954f4bf90f8de4271ef8866aae526ba5e
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2024-11-17 22:15:43 -0400

    Controlled and uncontrolled Components

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("");
   return (
     <div>
+      <input 
+        type="text"
+        value={value}
+        onChange={(e)=>setValue(e.target.value)}
+        placeholder="Ingresa el codigo del cupon. Ej: 100FF"
+      />
+      <p>El cupon es {value}</p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,20 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      alert(`Nuevo producto añadido: ${inputRef.current.value}`);
+    }
+  }
   return (
     <div>
+      <input 
+        ref={inputRef}
+        type="text" 
+        placeholder="Name of the purchase. e.g. Apple"
+      />
+      <button onClick={handleSubmit}>add to cart</button>
     </div>
   );
 };
