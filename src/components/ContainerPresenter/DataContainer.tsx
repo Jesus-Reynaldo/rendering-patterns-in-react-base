76fc19abbc8661043d13c802684123464a999b0f
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2024-11-17 22:02:30 -0400

    Container-Presenter

--- src/components/ContainerPresenter/DataContainer.tsx
+++ src/components/ContainerPresenter/DataContainer.tsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
+import { DataPresenter } from "./DataPresenter";
+interface DataItem {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
 export const DataContainer = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data/data.json");
+        if (!response.ok) {
+          throw new Error("Error something went wrong");
+        }
+        const result = await response.json();
+        setData(result);
+      }
+      catch (error) {
+        setError(`Error: ${error}`);
+      }
+      finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <div>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <DataPresenter data={data} />
     </div>
   );
 };
--- src/components/ContainerPresenter/DataPresenter.tsx
+++ src/components/ContainerPresenter/DataPresenter.tsx
@@ -1,6 +1,23 @@
-export const DataPresenter = () => {
+import React from "react";
+
+interface DataItem {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
+interface DataPresenterProps {
+  data: DataItem[];
+}
+export const DataPresenter = ({data}:DataPresenterProps) => {
   return (
     <>
+    {data.map((item) => (
+      <React.Fragment key={item.id}>
+        <img src={item.image} alt={item.name} />
+        <em>{item.description}</em>
+      </React.Fragment>
+    ))}
     </>
   );
 };
