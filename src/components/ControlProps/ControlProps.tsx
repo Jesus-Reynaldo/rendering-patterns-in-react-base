c10f49acd899283f73e339f4173bd2da187f6edc
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2024-11-17 20:28:15 -0400

    Control Props

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
+interface ToggleProps {
+  isToggled?: boolean;
+  onToggle?: (value: boolean) => void;
+}
+export const Toggle = ({isToggled=false, onToggle}:ToggleProps) => {
+  const [internalToggle, setInternalToggle] = useState(isToggled);
+  
+  const toggle = () => {
+    const newToggle = !internalToggle;
+    setInternalToggle(newToggle);
+    if (onToggle) {
+      onToggle(newToggle);
+    }
+  }
+
+  return (
+    <button onClick={toggle}>
+      {internalToggle ? "ON 🙄" : "OFF 😣"}
+    </button>
+  );
+};
 export const ParentComponent = () => {
+  const [toggleState, setToggleState] = useState(false);
   return (
     <div>
+      <p>Toggle is {toggleState ? "ON 🙄" : "OFF 😣"}</p>
+      <Toggle isToggled={toggleState} onToggle={setToggleState} />
     </div>
   );
 };
