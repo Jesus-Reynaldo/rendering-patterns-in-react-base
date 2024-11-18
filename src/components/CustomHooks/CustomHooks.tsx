5ce53e49a2cd6f656c0a4d4aad64c46c8f04531c
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2024-11-17 21:40:00 -0400

    Custom Hooks

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,30 @@
-const UserComponent = () => {
+import { useState } from "react";
+
+interface UserProps {
+  name: string;
+}
+function useLoading(initiaLoading: boolean = false) {
+  const [isLoading, setIsLoading] = useState(initiaLoading);
+  return {
+    isLoading,
+    setIsLoading,
+  };
+}
+
+const UserComponent = ({name}:UserProps) => {
+  const {isLoading, setIsLoading} = useLoading(false);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
+      <p>Hello, {name}!</p>
+      <button onClick={() => setIsLoading(true)}>Simulate Loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name="Teffcode" />;
 };
+
