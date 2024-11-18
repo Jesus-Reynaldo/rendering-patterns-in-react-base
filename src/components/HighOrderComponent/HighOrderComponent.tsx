822eea761197118adf0e7c7ba286e5991181ca6a
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2024-11-17 21:30:33 -0400

    Higher Order Components

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,23 @@
+interface UserProps {
+  name: string;
+}
+function withLoading<T extends object>(Component: React.ComponentType<T>) {
+  return (props: T & {isLoading: boolean}) => {
+    const {isLoading, ...rest} = props;
+    return isLoading ? <p>Loading...</p> : <Component {...rest as T} />;
+  };
+}
+
+const UserCompponent = ({name}:UserProps) => {
+  return <p>Hello, {name}!</p>;
+}
+
+const UserWithLoading = withLoading(UserCompponent);
 export const ParentComponent = () => {
   return (
     <div>
+      <UserWithLoading name="Teffcode" isLoading={true} />
+      <UserWithLoading name="Teffcode" isLoading={false} />
     </div>
   );
 };
